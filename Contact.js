241473cbd847efad18be8183d3b2170ad2e48632
Amani Louendriz <aloue024@uottawa.ca>
2021-10-25 17:07:16 -0400

    Updated Contact

--- Contact.js
+++ Contact.js
@@ -4,9 +4,23 @@ function validation() {
     var phone=document.getElementById("phone").value;
     var email=document.getElementById("email").value;
     var message=document.getElementById("Message").value;
-    var error_message=document.getElementById("error_message")
 
     alert(name);
-}
+};
+function valideMail(){
+    let Email=document.getElementById("email").value
+    for (let i = 0;i<Email.length;i++) {
+        if (Email[i] == '@') {
+            return alert("Your email is valid!")
+        }
+        else {
+            alert("Error!Invalid email!")
+        }
+    }
+};
+var btn=document.querySelector("btn")
+btn.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("here")
 
-validation();
+});
