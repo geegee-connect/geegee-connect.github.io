e73be7ae02537615a6ab40c279e4bd9ecf173e20
Amani Louendriz <aloue024@uottawa.ca>
2021-10-24 23:10:13 -0400

    Struggled to understand why js isn't working

---
+++ Contact.js
@@ -0,0 +1,10 @@
+function validation() {
+    var name=document.getElementById("name").value;
+    var subject=document.getElementById("subject").value;
+    var phone=document.getElementById("phone").value;
+    var email=document.getElementById("email").value;
+    var message=document.getElementById("Message").value;
+    var error_message=document.getElementById("error_message")
+
+    alert(name);
+}
