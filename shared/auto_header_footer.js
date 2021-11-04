ba18031247f3f35b10e71f37077ab10b92d9e48a
Antonin <90482816+antoninfrancoeur@users.noreply.github.com>
2021-11-04 03:07:57 -0400

    I wasted hours of my time I will never get back. I wasn't able to finish and it's now 3 am

--- shared/auto_header_footer.js
+++ shared/auto_header_footer.js
@@ -6,29 +6,53 @@ function setupHeaderAndFooter () {
     // Appends the top navigation bar to any pages with this script
     const template_header = document.createElement('div');
     template_header.innerHTML = `
-    <div class="topnav-bg">
-    <div class="container" style="max-width: 800px; margin: 0 auto;">
-    <div class="topnav">
-        <div class="topnav_buttons_img"> <img src = "images/LogoWhiteNoText.svg" alt="Logo" img draggable="false"/> </div>
-        <div class="topnav_buttons"> <a href="./index.html">Home</a> </div>
-        <div class="topnav_buttons"> <a href="./features.html">Features</a> </div>
-        <div class="topnav_buttons"> <a href="./about.html">About us</a> </div>
-        <div class="topnav_buttons"> <a href="#contact">Contact</a> </div>
-    </div> 
-    </div>
+    <div class="navbar">
+        <div class="container">
+            <a class="logo" href="#">Uni<span>Network</span></a>
+
+            <img class="mobile-menu" id="mobile-cta" src="images/menu.svg" alt="Open Navigation Bar">
+
+            <nav>
+                <img src="images/exit.svg" id="mobile-exit" class="mobile-menu-exit" alt="Close Navigation Bar">
+
+                <ul class="primary-nav">
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="./features.html">Features</a></li>
+                    <li><a href="#">Pricing</a></li>
+                </ul>
+
+                <ul class="secondary-nav">
+                    <li><a href="./about.html">Team</a></li>
+                    <li class="download-now-cta"><a href="#">Download now</a></li>
+                </ul>
+            </nav>
+        </div>
     </div>
     `;
     document.body.prepend(template_header);
 
+    // 1. Check in what page we are.
+    // 2. Add class="current" to the correct <li>
+
+    var elem = document.getElementsByTagName("li");
+    for (var i = 0; i < elem.length; i++) {
+
+        // Make add class current to "li" if its url match the current page
+        var listElemUrl = elem[i].getElementsByTagName("a")[0].href;
+        if(listElemUrl == window.location.href)
+            elem[i].classList.add("current");
+    }
+
     // Appends the bottom footer bar to any pages with this script
     const template_footer = document.createElement('div');
     template_footer.innerHTML = `
-    <div class="footer">
-        <a href="">Terms of service<br></a>
-        <a href="">Contact us<br></a>
-        <a href="">Privacy Policy<br></a>
-        <p>This is not a real product.</p>
-    </div>`;
+    <footer>
+		<div class="footerc">
+			<h6>Privacy Policy | Terms of Use</h6>
+			<h6>Copyright &#169 2021 UniNetwork. All rights reserved.</h6>
+		</div>
+	</footer>
+    `;
     document.body.append(template_footer);
 }
 
