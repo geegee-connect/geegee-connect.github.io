95169a531fb37b45d3afad8853090ec2c557663b
Antonin <90482816+antoninfrancoeur@users.noreply.github.com>
2021-11-17 01:36:34 -0500

    Changed the content of app_display.html

--- app_display.js
+++ app_display.js
@@ -7,13 +7,14 @@ window.addEventListener('resize', onResize, true);
 function onResize () {
     var width = overlay.offsetWidth;
     var height = overlay.offsetHeight;
-    var cropX = 0.182 * width; //0.182
+    var cropX = 0.1845 * width; //0.182
     var cropY = 0.077 * height; //0.080
+    var offsetX = -0.00 * width;
     var offsetY = 0.013 * height;
     var viewportOffset = overlay.getBoundingClientRect();
     
     base.style.width = (overlay.offsetWidth - cropX) + "px";
     base.style.height = (overlay.offsetHeight - cropY) + "px";
-    base.style.left = (viewportOffset.left + (cropX * 0.5)) + "px";
+    base.style.left = (viewportOffset.left + (cropX * 0.5) - offsetX) + "px";
     base.style.top = (viewportOffset.top + (cropY * 0.5) - offsetY) + "px";
 }
